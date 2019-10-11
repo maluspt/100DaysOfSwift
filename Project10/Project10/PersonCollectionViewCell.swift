3f730c9758a645b6afc021783795ca9fc9b2497e
Maria Luiza <32915782+maluspt@users.noreply.github.com>
2019-10-11 11:25:33 -0300

    Add files via upload

---
+++ Project10/Project10/PersonCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCollectionViewCell.swift
+//  Project10
+//
+//  Created by Maria Luiza Carvalho Sperotto on 07/10/19.
+//  Copyright © 2019 Maria Luiza Carvalho Sperotto. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personImage: UIImageView!
+}
