d2750db93e3cb8d6c1161f1171820465a6bf97cd
Maria Luiza <32915782+maluspt@users.noreply.github.com>
2019-11-04 16:03:38 -0300

    Project 19 | Javascript Injections
    No challenges included

---
+++ Project19/Extension/ActionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Maria Luiza Carvalho Sperotto on 04/11/19.
+//  Copyright © 2019 Maria Luiza Carvalho Sperotto. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    var pageTitle = ""
+    var pageURL = ""
+    
+    @IBOutlet weak var script: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
+                        
+                }
+            }
+        }
+    
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        script.scrollIndicatorInsets = script.contentInset
+
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+
+}
---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Maria Luiza Carvalho Sperotto on 04/11/19.
+//  Copyright © 2019 Maria Luiza Carvalho Sperotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
